d27bd6234724f9a3dc24cf89217d436708e1f31d
Amr Emara <a.emara47@gmail.com>
2022-08-01 11:52:29 +0200

    setup main layout for mobile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TitlebarComponent } from './components/titlebar/titlebar.component';
+import { ColumnsComponent } from './components/columns/columns.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitlebarComponent,
+    ColumnsComponent
   ],
   imports: [
     BrowserModule
