3362045fa2dc19006fd8de15760429b2518568d7
Amr Emara <a.emara47@gmail.com>
2022-08-08 22:37:08 +0200

    init BoardsModal component

---
+++ src/app/components/boards-modal/boards-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-boards-modal',
+  templateUrl: './boards-modal.component.html',
+  styleUrls: ['./boards-modal.component.css']
+})
+export class BoardsModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
