c204704022975c77439db527e23d783f4eb6f9e1
Amr Emara <a.emara47@gmail.com>
2022-08-08 17:41:28 +0200

    generate DeleteModal component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TitlebarComponent } from './components/titlebar/titlebar.component';
 import { ColumnsComponent } from './components/columns/columns.component';
+import { DeleteModalComponent } from './components/delete-modal/delete-modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TitlebarComponent,
-    ColumnsComponent
+    ColumnsComponent,
+    DeleteModalComponent
   ],
   imports: [
     BrowserModule
