48bae68c1750ff756b241bd4edce2b5720f35bfb
Amr Emara <a.emara47@gmail.com>
2022-08-27 02:52:22 +0200

    fix variables names

--- src/app/components/column/column.component.ts
+++ src/app/components/column/column.component.ts
@@ -18,10 +18,10 @@ export class ColumnComponent implements OnInit {
 
   ngOnInit(): void {
     this.columnName = this.column.title;
-    this.columnId = this.column.id;
-    this.http.getTasks(this.columnId).subscribe((tasks) => {
-      this.tasks = tasks;
-      this.numOfTasks = tasks.length;
+    this.columnId = this.column._id;
+    this.http.getTasks(this.columnId).subscribe((data) => {
+      this.tasks = data.tasks;
+      this.numOfTasks = this.tasks.length;
     })
   }
 
