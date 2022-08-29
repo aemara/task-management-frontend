7d416771f250eaae2a3923ab4695e05adeb9c244
Amr Emara <a.emara47@gmail.com>
2022-08-29 23:40:51 +0200

    add columns dropdown menu and its logic

--- src/app/components/view-task/view-task.component.ts
+++ src/app/components/view-task/view-task.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { HttpService } from 'src/app/services/http.service';
+import { UiService } from 'src/app/services/ui.service';
 
 @Component({
   selector: 'app-view-task',
@@ -12,40 +13,69 @@ export class ViewTaskComponent implements OnInit {
   taskTitle!: string;
   taskDescription!: string;
   currentColumn!: string;
+  columns!: any[];
   columnId!: string;
+  boardId!: string;
   subtasks = [];
   numOfSubtasks!: number;
   numOfSubtasksDone = 0;
-  
+  showDropdown: boolean = false;
 
-  constructor(private route: ActivatedRoute, private http: HttpService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpService,
+    private ui: UiService
+  ) {}
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
       this.taskId = params['id'];
     });
 
-    this.http.getTask(this.taskId).subscribe(data => {
+    this.boardId = this.ui.boardId;
+
+    this.http.getColumns(this.boardId).subscribe((data) => {
+      this.columns = data.columns;
+    });
+
+    this.http.getTask(this.taskId).subscribe((data) => {
       this.taskTitle = data.task[0].title;
       this.taskDescription = data.task[0].description;
       this.currentColumn = data.task[0].column;
       this.columnId = data.task[0].columnId;
-    })
+      this.http.getColumn(data.task[0].columnId).subscribe((data) => {
+        this.currentColumn = data.column[0].title;
+      });
+    });
 
-    this.http.getSubtasks(this.taskId).subscribe(data => {
+    this.http.getSubtasks(this.taskId).subscribe((data) => {
       this.subtasks = data.subtasks;
       this.numOfSubtasks = this.subtasks.length;
-      this.subtasks.forEach((subtask:any) => {
-        if(subtask.done) this.numOfSubtasksDone++;
-      })
-    })  
+      this.subtasks.forEach((subtask: any) => {
+        if (subtask.done) this.numOfSubtasksDone++;
+      });
+    });
   }
 
   updateDoneCount(input: string) {
-    if(input === 'true') {
+    if (input === 'true') {
       this.numOfSubtasksDone++;
     } else {
       this.numOfSubtasksDone--;
+    }
+  }
+
+  changeCurrentColumn(column: string, columnId: string) {
+    this.currentColumn = column;
+    this.http.changeColumn(this.taskId, columnId).subscribe();
+    this.showDropdown = false;
+  }
+
+  toggleDropdown() {
+    if (this.showDropdown) {
+      this.showDropdown = false;
+    } else {
+      this.showDropdown = true;
     }
   }
 }
