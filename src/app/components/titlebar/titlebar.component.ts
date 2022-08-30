779e8fe2e0b520b7103b09b6abfaaf01affff55b
Amr Emara <a.emara47@gmail.com>
2022-08-30 22:17:20 +0200

    add options menu for editing/deleting board

--- src/app/components/titlebar/titlebar.component.ts
+++ src/app/components/titlebar/titlebar.component.ts
@@ -1,26 +1,35 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { UiService } from 'src/app/services/ui.service';
 
 @Component({
   selector: 'app-titlebar',
   templateUrl: './titlebar.component.html',
-  styleUrls: ['./titlebar.component.css']
+  styleUrls: ['./titlebar.component.css'],
 })
 export class TitlebarComponent implements OnInit {
-
-  @Output() showBoardsModal  = new EventEmitter<any>();
+  @Output() showBoardsModal = new EventEmitter<any>();
   boardName!: string;
+  boardId!: string;
+  showOptions: boolean = false;
 
-  constructor(private uiService: UiService) { }
+  constructor(private uiService: UiService) {}
 
   ngOnInit(): void {
-    this.uiService.changeEmitted$.subscribe(board => {
+    this.uiService.changeEmitted$.subscribe((board) => {
       this.boardName = board.title;
-  });
+      this.boardId = board._id;
+    });
   }
 
   onShowBoardsModal() {
     this.showBoardsModal.emit();
   }
 
+  toggleOptions() {
+    if (this.showOptions) {
+      this.showOptions = false;
+    } else {
+      this.showOptions = true;
+    }
+  }
 }
