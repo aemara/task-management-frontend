4eb50374473e1adaaae003134f693dcb93a2c8f0
Amr Emara <a.emara47@gmail.com>
2022-09-14 04:15:38 +0200

    remove unecessary http request

--- src/app/components/column/column.component.ts
+++ src/app/components/column/column.component.ts
@@ -1,28 +1,25 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { HttpService } from 'src/app/services/http.service';
+import { UiService } from 'src/app/services/ui.service';
 
 @Component({
   selector: 'app-column',
   templateUrl: './column.component.html',
-  styleUrls: ['./column.component.css']
+  styleUrls: ['./column.component.css'],
 })
 export class ColumnComponent implements OnInit {
-
   @Input() column: any;
   columnName!: string;
   columnId!: number;
   tasks!: any[];
   numOfTasks!: number;
-  
-  constructor(private http: HttpService) { }
+
+  constructor(private http: HttpService, private uiService: UiService) {}
 
   ngOnInit(): void {
     this.columnName = this.column.title;
     this.columnId = this.column._id;
-    this.http.getTasks(this.columnId).subscribe((data) => {
-      this.tasks = data.tasks;
-      this.numOfTasks = this.tasks.length;
-    })
+    this.tasks = this.column.tasks;
+    this.numOfTasks = this.tasks.length;
   }
-
 }
