cb2082dd78b503990cd87d324d276d91f6cbb6fe
Amr Emara <a.emara47@gmail.com>
2022-09-22 19:56:49 +0200

    add logic of updating tasks

--- src/app/components/add-edit-task/add-edit-task.component.ts
+++ src/app/components/add-edit-task/add-edit-task.component.ts
@@ -1,8 +1,10 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormGroup, FormArray, FormControl, Validators } from '@angular/forms';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { map } from 'rxjs';
 import { HttpService } from 'src/app/services/http.service';
 import { UiService } from 'src/app/services/ui.service';
+import * as _ from 'lodash';
 @Component({
   selector: 'app-add-edit-task',
   templateUrl: './add-edit-task.component.html',
@@ -17,6 +19,12 @@ export class AddEditTaskComponent implements OnInit {
   selectedColumnId!: string;
   displayDropdown: boolean = false;
   boardId!: string;
+  taskId!: string;
+  currentTaskTitle!: string;
+  currentTaskDescription!: string;
+  currentTaskSubtasks!: any;
+  currentTaskColumnId!: string;
+  currentTaskColumn!: string;
   constructor(
     private http: HttpService,
     private uiService: UiService,
@@ -34,17 +42,54 @@ export class AddEditTaskComponent implements OnInit {
         title: new FormControl(null, Validators.required),
         description: new FormControl(null, Validators.required),
         subtasks: new FormArray([
-          new FormControl(null, Validators.required),
-          new FormControl(null, Validators.required),
+          new FormGroup({
+            subtask: new FormControl(null, Validators.required),
+            done: new FormControl(false),
+          }),
+          new FormGroup({
+            subtask: new FormControl(null, Validators.required),
+            done: new FormControl(false),
+          }),
         ]),
         column: new FormControl(null, Validators.required),
+      });
+    } else {
+      this.uiService.taskDisplay$.subscribe((taskData) => {
+        this.taskId = taskData.task._id;
+        this.currentTaskTitle = taskData.task.title;
+        this.currentTaskDescription = taskData.task.description;
+        this.currentTaskSubtasks = taskData.task.subtasks;
+        this.currentTaskColumnId = taskData.columnId;
+        this.currentTaskColumn = taskData.columnName;
+        this.selectedColumn = taskData.columnName;
+      });
+
+      const currentSubtasksFormControls: any = [];
+      this.currentTaskSubtasks.forEach((subtask: any) => {
+        const formGroup = new FormGroup({
+          subtask: new FormControl(subtask.name, Validators.required),
+          done: new FormControl(subtask.done),
+        });
+        currentSubtasksFormControls.push(formGroup);
+      });
+      this.taskForm = new FormGroup({
+        title: new FormControl(this.currentTaskTitle, Validators.required),
+        description: new FormControl(
+          this.currentTaskDescription,
+          Validators.required
+        ),
+        subtasks: new FormArray(currentSubtasksFormControls),
+        column: new FormControl(this.currentTaskColumn, Validators.required),
       });
     }
   }
 
   onAddSubtask() {
-    const control = new FormControl(null, Validators.required);
-    (<FormArray>this.taskForm.get('subtasks')).push(control);
+    const group = new FormGroup({
+      subtask: new FormControl(null, Validators.required),
+      done: new FormControl(false),
+    });
+    (<FormArray>this.taskForm.get('subtasks')).push(group);
   }
 
   getSubtasksControls() {
@@ -77,25 +122,51 @@ export class AddEditTaskComponent implements OnInit {
   }
 
   onSubmit() {
-    const subtasks: any[] = [];
-    if (this.taskForm.value.subtasks.length > 0) {
-      this.taskForm.value.subtasks.forEach((subtaskName: string) => {
-        const subtask = { name: subtaskName };
+    if (this.addOrEditTask === 'add') {
+      const subtasks: any[] = [];
+      if (this.taskForm.value.subtasks.length > 0) {
+        this.taskForm.value.subtasks.forEach((subtaskObject: any) => {
+          const subtask = {
+            name: subtaskObject.subtask,
+            done: subtaskObject.done,
+          };
+          subtasks.push(subtask);
+        });
+      }
+
+      const task = {
+        title: this.taskForm.value.title,
+        description: this.taskForm.value.description,
+        subtasks: subtasks,
+      };
+
+      this.http.addTask(task, this.selectedColumnId).subscribe(() => {
+        this.hideAddEditTask.emit();
+        this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+        this.router.onSameUrlNavigation = 'reload';
+        this.router.navigate([this.router.url]);
+      });
+    } else {
+      const newData: any = {};
+      const subtasks: any = [];
+      newData['newTitle'] = this.taskForm.value.title;
+      newData['newDescription'] = this.taskForm.value.description;
+      this.taskForm.value.subtasks.forEach((subtaskObject: any) => {
+        const subtask = {
+          name: subtaskObject.subtask,
+          done: subtaskObject.done,
+        };
         subtasks.push(subtask);
       });
+      newData['newSubtasks'] = subtasks;
+      if (this.selectedColumnId !== this.currentTaskColumnId) {
+        newData['newColumnId'] = this.selectedColumnId;
+        newData['currentColumnId'] = this.currentTaskColumnId;
+      }
+      this.http.editTask(this.taskId, newData).subscribe(() => {
+        this.hideAddEditTask.emit();
+        this.router.navigate([this.router.url]);
+      });
     }
-
-    const task = {
-      title: this.taskForm.value.title,
-      description: this.taskForm.value.description,
-      subtasks: subtasks,
-    };
-
-    this.http.addTask(task, this.selectedColumnId).subscribe(() => {
-      this.hideAddEditTask.emit();
-      this.router.routeReuseStrategy.shouldReuseRoute = () => false;
-      this.router.onSameUrlNavigation = 'reload';
-      this.router.navigate([this.router.url]);
-    });
   }
 }
