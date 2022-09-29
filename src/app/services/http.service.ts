0fe48d9037711d3577a3af38086909ff81cff99f
Amr Emara <a.emara47@gmail.com>
2022-09-29 06:29:50 +0200

    change requests' paths

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -8,75 +8,94 @@ export class HttpService {
   constructor(private http: HttpClient) {}
 
   addBoard(data: any) {
-    return this.http.post<any>(`http://localhost:3000/addboard`, data);
+    return this.http.post<any>(`http://localhost:3000/boards/addboard`, data);
   }
 
   addColumn(data: any, boardId: string) {
     return this.http.post<any>(
-      `http://localhost:3000/addcolumn/${boardId}`,
+      `http://localhost:3000/columns/addcolumn/${boardId}`,
       data
     );
   }
 
   addTask(data: any, columnId: string) {
     return this.http.post<any>(
-      `http://localhost:3000/addtask/${columnId}`,
+      `http://localhost:3000/tasks/addtask/${columnId}`,
       data
     );
   }
 
   getBoards() {
-    return this.http.get<any>('http://localhost:3000/boards');
+    return this.http.get<any>('http://localhost:3000/boards/getboards');
   }
 
   getBoard(boardId: string) {
-    return this.http.get<any>(`http://localhost:3000/board/${boardId}`);
+    return this.http.get<any>(
+      `http://localhost:3000/boards/getboard/${boardId}`
+    );
   }
 
   getColumns(boardId: string) {
-    return this.http.get<any>(`http://localhost:3000/columns/${boardId}`);
+    return this.http.get<any>(
+      `http://localhost:3000/columns/getcolumns/${boardId}`
+    );
   }
 
   getColumn(columnId: string) {
-    return this.http.get<any>(`http://localhost:3000/column/${columnId}`);
+    return this.http.get<any>(
+      `http://localhost:3000/columns/getcolumn/${columnId}`
+    );
   }
 
   getTasks(columnId: number) {
-    return this.http.get<any>(`http://localhost:3000/tasks/${columnId}`);
+    return this.http.get<any>(
+      `http://localhost:3000/tasks/gettasks/${columnId}`
+    );
   }
 
   getTask(taskId: string) {
-    return this.http.get<any>(`http://localhost:3000/task/${taskId}`);
+    return this.http.get<any>(`http://localhost:3000/tasks/gettask/${taskId}`);
   }
 
   getSubtasks(taskId: string) {
-    return this.http.get<any>(`http://localhost:3000/subtasks/${taskId}`);
+    return this.http.get<any>(`http://localhost:3000/subtasks/getsubtasks/${taskId}`);
   }
 
   editBoard(boardId: string, data: any) {
-    return this.http.put<any>(`http://localhost:3000/editboard/${boardId}`, data);
+    return this.http.put<any>(
+      `http://localhost:3000/boards/updateboard/${boardId}`,
+      data
+    );
   }
 
   toggleSubtaskStatus(subtaskId: string, taskId: string) {
-    return this.http.put(`http://localhost:3000/toggledone/${subtaskId}/${taskId}`, {});
+    return this.http.put(
+      `http://localhost:3000/subtasks/toggledone/${subtaskId}/${taskId}`,
+      {}
+    );
   }
 
   changeColumn(taskId: string, currentColumnId: string, newColumnId: string) {
     return this.http.put(
-      `http://localhost:3000/changecolumn/${taskId}/${currentColumnId}/${newColumnId}`,
+      `http://localhost:3000/tasks/changecolumn/${taskId}/${currentColumnId}/${newColumnId}`,
       {}
     );
   }
 
   editTask(taskId: string, data: any) {
-    return this.http.put(`http://localhost:3000/edittask/${taskId}`, data);
+    return this.http.put(
+      `http://localhost:3000/tasks/updatetask/${taskId}`,
+      data
+    );
   }
 
   deleteBoard(boardId: string) {
-    return this.http.delete(`http://localhost:3000/deleteboard/${boardId}`);
+    return this.http.delete(
+      `http://localhost:3000/boards/deleteboard/${boardId}`
+    );
   }
 
   deleteTask(taskId: string) {
-    return this.http.delete(`http://localhost:3000/deletetask/${taskId}`);
+    return this.http.delete(`http://localhost:3000/tasks/deletetask/${taskId}`);
   }
 }
