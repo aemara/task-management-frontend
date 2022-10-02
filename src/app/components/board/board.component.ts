e05683adc129625571b746d2eb766ad856fe240c
Amr Emara <a.emara47@gmail.com>
2022-10-02 08:51:54 +0200

    add error handling

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { HttpService } from 'src/app/services/http.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { UiService } from 'src/app/services/ui.service';
 @Component({
   selector: 'app-board',
@@ -14,11 +14,13 @@ export class BoardComponent implements OnInit {
   columns: any[] = [];
   isFetching: boolean = false;
   areThereBoards!: boolean;
+  error!: any;
 
   constructor(
     private httpService: HttpService,
     private route: ActivatedRoute,
-    private uiService: UiService
+    private uiService: UiService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -28,30 +30,44 @@ export class BoardComponent implements OnInit {
       this.isFetching = true;
       if (this.boardId) {
         this.areThereBoards = true;
-        this.httpService.getBoard(this.boardId).subscribe((data) => {
-          this.board = data.board;
-          this.columns = this.board.columns;
-          this.uiService.emitChange(this.board);
-          this.isFetching = false;
-        });
+        this.httpService.getBoard(this.boardId).subscribe(
+          (data) => {
+            this.board = data.board;
+            this.columns = this.board.columns;
+            this.uiService.emitChange(this.board);
+            this.isFetching = false;
+          },
+          (error) => {
+            console.log(error);
+            this.error = error;
+            this.isFetching = false;
+          }
+        );
       } else {
         /**Else fetch the last added board on initial app load */
         this.isFetching = true;
-        this.httpService.getBoard('-1').subscribe((data) => {
-          if (data) {
-            this.board = data.board[0];
-            if (this?.board) {
-              this.areThereBoards = true;
-              this.columns = this.board.columns;
-              this.uiService.emitChange(this.board);
-            } else {
-              /**If there are no boards */
-              this.areThereBoards = false;
-              this.uiService.emitNoBoards(null);
+        this.httpService.getBoard('-1').subscribe(
+          (data) => {
+            if (data) {
+              this.board = data.board[0];
+              if (this?.board) {
+                this.areThereBoards = true;
+                this.columns = this.board.columns;
+                this.uiService.emitChange(this.board);
+              } else {
+                /**If there are no boards */
+                this.areThereBoards = false;
+                this.uiService.emitNoBoards(null);
+              }
             }
+            this.isFetching = false;
+          },
+          (error) => {
+            console.log(error);
+            this.error = error;
+            this.isFetching = false;
           }
-          this.isFetching = false;
-        });
+        );
       }
     });
   }
@@ -62,5 +78,11 @@ export class BoardComponent implements OnInit {
 
   onClickAddBoard() {
     this.uiService.showAddBoard('');
+  }
+
+  refetchBoards() {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload';
+    this.router.navigate([this.router.url]);
   }
 }
