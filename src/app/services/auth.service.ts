f5b42b7a6c48caac74ba035fca9397ba447a4178
Amr Emara <a.emara47@gmail.com>
2022-12-10 05:33:38 +0200

    add automatic logging out

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { BehaviorSubject, Subject } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { User } from '../models/user.model';
@@ -8,7 +9,9 @@ import { User } from '../models/user.model';
   providedIn: 'root',
 })
 export class AuthService {
+  logOutTimeout!: any;
   userSubject = new BehaviorSubject<any>(null);
+  jwtHelper = new JwtHelperService();
   constructor(private http: HttpClient) {}
 
   signUp(username: string, password: string) {
@@ -21,6 +24,10 @@ export class AuthService {
         tap((data: any) => {
           const user = new User(data.username, data.id, data.accessToken);
           this.userSubject.next(user);
+          const timeTillExpiry =
+            this.jwtHelper.getTokenExpirationDate(user.getToken)!.getTime() -
+            new Date().getTime();
+          this.autoLogout(timeTillExpiry);
           localStorage.setItem('userData', JSON.stringify(user));
         })
       );
@@ -36,6 +43,10 @@ export class AuthService {
         tap((data: any) => {
           const user = new User(data.username, data.id, data.accessToken);
           this.userSubject.next(user);
+          const timeTillExpiry =
+            this.jwtHelper.getTokenExpirationDate(user.getToken)!.getTime() -
+            new Date().getTime();
+          this.autoLogout(timeTillExpiry);
           localStorage.setItem('userData', JSON.stringify(user));
         })
       );
@@ -46,10 +57,23 @@ export class AuthService {
     if (userData) {
       const user = new User(userData.username, userData.id, userData.token);
       this.userSubject.next(user);
+      const timeTillExpiry =
+        this.jwtHelper.getTokenExpirationDate(user.getToken)!.getTime() -
+        new Date().getTime();
+      this.autoLogout(timeTillExpiry);
     }
+  }
+
+  autoLogout(timeTillExpiry: number) {
+    this.logOutTimeout = setTimeout(() => {
+      this.logOut();
+    }, timeTillExpiry);
   }
 
   logOut() {
     localStorage.removeItem('userData');
+    this.userSubject.next(null);
+    clearTimeout(this.logOutTimeout);
+    this.logOutTimeout = null;
   }
 }
